27996661766e41c03b0990e7b8c736ec52c5499f
pavel.nikitsiuk <pnikitsiuk@demandbase.com>
2021-05-05 11:04:54 +0300

    implement requests to server

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,7 +6,7 @@ import { v4 as uuidv4 } from "uuid";
 
 interface CardInfo {
   title: string;
-  description: string;
+  agenda: string;
 }
 
 const useStyles = makeStyles({
@@ -33,7 +33,7 @@ const useStyles = makeStyles({
   },
 });
 
-const initCardInfo = { title: "", description: "" };
+const initCardInfo = { title: "", agenda: "" };
 
 export default function Header() {
   const classes = useStyles();
@@ -41,63 +41,63 @@ export default function Header() {
   const [open, setOpen] = useState(false);
   const [cardInfo, setCardInfo] = useState<CardInfo>(initCardInfo);
 
-  const { addCard } = useActions();
+  const { createMeetings } = useActions();
 
   const handleChangeInput = useCallback(
-    (event: ChangeEvent<HTMLInputElement>) =>
-      setCardInfo((prevCardInfo) => {
-        return {
-          ...prevCardInfo,
-          [event.target.name]: event.target.value,
-        };
-      }),
-    []
+      (event: ChangeEvent<HTMLInputElement>) =>
+          setCardInfo((prevCardInfo) => {
+            return {
+              ...prevCardInfo,
+              [event.target.name]: event.target.value,
+            };
+          }),
+      []
   );
 
   const onSaveCard = () => {
     const newCard = {
-      id: uuidv4(),
-      // title: cardInfo.title,
-      // agenda: cardInfo.agenda,
+      title: cardInfo.title,
+      notes: cardInfo.agenda,
+      description: "qwerty",
+      participants: [{fullName: 'kek', lastName:'kek', isAdmin: true, isSpeaker: true}],
+      dates: [new Date().getUTCDate(), new Date().toLocaleTimeString("default", { month: "short" })],
       // day: new Date().getUTCDate(),
-      // month: new Date().toLocaleString("default", { month: "short" }),
-      title:  cardInfo.title,
-      description: cardInfo.description,
+      // month: new Date().toLocaleTimeString("default", { month: "short" }),
     };
-    addCard(newCard);
+    createMeetings(newCard);
     setOpen(false);
     setCardInfo(initCardInfo);
   };
 
   return (
-    <header className={classes.header}>
-      <h3 className="header_title">Tech Talks App</h3>
-      <Button onClick={() => setOpen(true)}> Add Card</Button>
-      <Dialog
-        open={open}
-        onClose={() => setOpen(false)}
-        classes={{
-          paper: classes.paper,
-        }}
-      >
-        <form action="">
-          <Input
-            label="Title"
-            name="title"
-            containerClasses={classes.input}
-            onChange={handleChangeInput}
-          />
-          <Input label="Agenda" name="agenda" onChange={handleChangeInput} />
-        </form>
-
-        <Button
-          onClick={onSaveCard}
-          disabled={!(!!cardInfo.title && !!cardInfo.description)}
+      <header className={classes.header}>
+        <h3 className="header_title">Tech Talks App</h3>
+        <Button onClick={() => setOpen(true)}> Add Card</Button>
+        <Dialog
+            open={open}
+            onClose={() => setOpen(false)}
+            classes={{
+              paper: classes.paper,
+            }}
         >
-          {" "}
-          save card
-        </Button>
-      </Dialog>
-    </header>
+          <form action="">
+            <Input
+                label="Title"
+                name="title"
+                containerClasses={classes.input}
+                onChange={handleChangeInput}
+            />
+            <Input label="Agenda" name="agenda" onChange={handleChangeInput} />
+          </form>
+
+          <Button
+              onClick={onSaveCard}
+              disabled={!(!!cardInfo.title && !!cardInfo.agenda)}
+          >
+            {" "}
+            save card
+          </Button>
+        </Dialog>
+      </header>
   );
 }
